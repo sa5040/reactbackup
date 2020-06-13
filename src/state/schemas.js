4ae27c40a16bcf9c99f6f452f8eeb5194507552f
sa5040 <sr.sanjeev@hotmail@.com>
2020-06-13 22:23:33 +0530

    changes for json server

---
+++ src/state/schemas.js
@@ -0,0 +1,33 @@
+import { schema } from "normalizr";
+
+export const specialist = new schema.Entity("specialists");
+
+export const channel = new schema.Entity("channels", {
+  specialists: [specialist]
+});
+
+export const channels = new schema.Entity(
+  "channels",
+  {},
+  {
+    processStrategy: entity => {
+      debugger;
+      entity.specialists.reverse();
+      return entity;
+    }
+  }
+);
+
+export const team = new schema.Entity("teams", {
+  channels: [channel]
+});
+
+export const teams = new schema.Entity("teams", {
+  teams: [team]
+});
+
+export const epic = new schema.Entity("epics");
+
+export const project = new schema.Entity("projects", {});
+
+export const task = new schema.Entity("tasks", {});
